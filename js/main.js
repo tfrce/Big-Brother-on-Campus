fd00c7a562e8f288ac89c75ab6a34d10fde87be3
Grant J. Gumina <grantgumina@gmail.com>
2013-08-19 21:24:52 -0400

    first pages commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#start-datetime-picker').datetimepicker({
+        language: 'en',
+        pick12HourFormat: true
+    });
+    
+     $('#end-datetime-picker').datetimepicker({
+        language: 'en',
+        pick12HourFormat: true
+    });
+});
